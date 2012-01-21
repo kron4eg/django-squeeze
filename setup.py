5a6cb3996857f09f12bba11d261bb3dcfa901247
Pavel Puchkin <neoascetic@gmail.com>
2012-01-21 23:22:29 +1100

    setup.py fixes

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 
 setup(name = "django-squeeze-gclosure",
       version = '0.2',
-      author = "Pavel Puchkin",
-      author_email = "neoascetic@gmail.com",
-      url = "http://github.com/neoascetic/django-squeeze-gclosure",
+      author = "Pavel Puchkin, Artiom Diomin",
+      author_email = "neoascetic@gmail.com, kron82@gmail.com",
+      url = "http://github.com/kron4eg/django-squeeze",
       license = "BSD",
       description = "Squeeze CSS and JS (using Crockford algorithm or Google Closure REST API) files on the fly, for django.",
       packages = ['gcsqueeze', 'gcsqueeze.templatetags'],
